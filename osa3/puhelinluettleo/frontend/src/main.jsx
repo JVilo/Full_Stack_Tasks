5b540c8533a3ba68aeaf54db20bde82abce4e24f
Jvilo <vilo.johanna@gmail.com>
2026-09-11 13:24:39 +0300

    debug changes

--- osa3/puhelinluettleo/frontend/src/main.jsx
+++ osa3/puhelinluettleo/frontend/src/main.jsx
@@ -1,10 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import axios from 'axios'
 import App from './App'
 import './style.css'
 
-const persons = axios.get('http://localhost:3001/api/persons').then(response =>{
-    const persons = response.data
-    console.log(persons)
-    ReactDOM.createRoot(document.getElementById('root')).render(<App persons={persons} />)
-})
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
