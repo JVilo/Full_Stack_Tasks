787afa6a4e3963eec9006325784ff0cd4c0ceb87
Jvilo <vilo.johanna@gmail.com>
2026-09-12 18:15:39 +0300

    task 3.15 -3.16

--- osa3/puhelinluettleo/index.js
+++ osa3/puhelinluettleo/index.js
@@ -26,25 +26,28 @@ app.use(requestLogger)
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.static('dist'))
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-  Person.find({}).then(persons => {
-    const info = `
-      <p>Phonebook has info for ${persons.length} people</p>
-      <p>${new Date()}</p>
-    `
-    response.send(info)
-  })
+app.get('/info', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      const info = `
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+      `
+      response.send(info)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  Person.findById(id)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
     .then(person => {
       if (person) {
         response.json(person)
@@ -52,12 +55,10 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()
       }
     })
-    .catch(error => {
-      response.status(500).send({ error: 'Failed to fetch person' })
-    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
@@ -75,28 +76,54 @@ app.post('/api/persons', (request, response) => {
     .then(savedPerson => {
       response.json(savedPerson)
     })
-    .catch(error => {
-      console.error(error)
-      response.status(500).json({ error: 'Failed to save person' })
-    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  Person.findByIdAndRemove(id)
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      if (updatedPerson) {
+        response.json(updatedPerson)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
     .then(() => {
       response.status(204).end()
     })
-    .catch(error => {
-      response.status(500).send({ error: 'Failed to delete person' })
-    })
+    .catch(error => next(error))
 })
 
+// Handler for unknown routes (must be loaded second to last)
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
+
+// Error handling middleware (must be loaded LAST)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
