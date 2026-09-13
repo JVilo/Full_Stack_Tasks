9866ab5a73eb66dadc5e1b9476c8003a4b31c1d4
Jvilo <vilo.johanna@gmail.com>
2026-09-13 14:52:22 +0300

    task 3.20

--- osa3/puhelinluettleo/models/persons.js
+++ osa3/puhelinluettleo/models/persons.js
@@ -20,7 +20,16 @@ const personSchema = new mongoose.Schema({
     minlength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    required: [true, 'User phone number required'],
+    validate: {
+      validator: function(v) {
+        return v.length >= 8 && /^\d{2,3}-\d+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
